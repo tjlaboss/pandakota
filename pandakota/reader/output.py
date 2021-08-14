5061f2f431b8c157c5a0477473039cab4788b946
Travis J. Labossiere-Hickman <travis@ktc-technology.com>
2021-08-14 10:29:20 -0400

    Read partial (rank) correlation coefficient matrices

--- pandakota/reader/output.py
+++ pandakota/reader/output.py
@@ -37,3 +37,49 @@ def read_partial_matrix(text: str) -> pd.DataFrame:
 		raise type(e)(f"Could not obtain read partial matrix from text: {e}")
 	return df
 
+
+def read_pearson_matrix(text, autosnip=True):
+	"""Read the Pearson Partial Correlation Matrix
+
+	Parameter:
+	----------
+	text: str
+		Text containing the partial matrix
+	
+	autosnip: bool; optional
+		Whether to detect the start and end of the
+		partial correlation matrix block using pandakota defaults.
+		[Default: True]
+
+	Returns:
+	--------
+	pd.DataFrame
+		DF of the partial correlation matrix
+	"""
+	if autosnip:
+		text = utils.snip_text(text, start=NAMES[PPCC])
+	return read_partial_matrix(text)
+
+
+def read_spearman_matrix(text, autosnip=True):
+	"""Read the Spearman Partial Rank Correlation Matrix
+
+	Parameter:
+	----------
+	text: str
+		Text containing the partial matrix
+
+	Returns:
+	--------
+	pd.DataFrame
+		DF of the partial rank correlation matrix
+	"""
+	if autosnip:
+		text = utils.snip_text(text, start=NAMES[SPRCC])
+	return read_partial_matrix(text)
+
+
+READERS = {
+	PPCC:  read_pearson_matrix,
+	SPRCC: read_spearman_matrix,
+}
