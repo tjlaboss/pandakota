cb2088cf2854f4ea711803bf166fc83e33da5746
tjlaboss <travis@ktc-technology.com>
2021-08-27 08:30:01 -0400

    Add a state variable

--- pandakota/input/variables.py
+++ pandakota/input/variables.py
@@ -4,17 +4,22 @@ Variables
 Module containing DAKOTA variable classes
 """
 import abc
+import typing
 
 class Variable(abc.ABC):
 	"""Abstract Base Class for DAKOTA input variable
 	
 	Parameters:
 	-----------
-	stream: str
-		Text of the partial matrix
+	key: str
+		Unique alphanumeric name of the variable
+		(underscores are OK).
+	
+	dtype: type
+		Required data type
 	"""
 	def __init__(self, key: str, dtype: type):
-		self.key = key
+		self.key = key  # TODO: ensure valid key names
 		self._dtype = dtype
 		self._element = None
 		
@@ -39,3 +44,42 @@ class Variable(abc.ABC):
 	@abc.abstractmethod
 	def _set_element(self, e):
 		pass
+	
+	@abc.abstractmethod
+	def _get_strings(self) -> typing.Tuple[str]:
+		pass
+
+
+class StateVariable(Variable):
+	"""Discrete state DAKOTA input variable
+
+	Parameters:
+	-----------
+	key: str
+		Unique alphanumeric name of the variable
+
+	dtype: type
+		Must be one of: {str, float, int}
+
+	"""
+	def __init__(self, key, dtype, element):
+		super().__init__(key, dtype)
+		self.element = element
+	
+	@classmethod
+	def fromVariable(cls, other):
+		"""Cast another variable as a StateVariable"""
+		return cls(other.key, other.dtype, other.element)
+	
+	def _set_element(self, e):
+		self._element = e
+	
+	def _get_strings(self):
+		namestr = '"{}"'.format(self.key)
+		if self.dtype is str:
+			elemstr = '"{}"'.format(self.element)
+		else:
+			elemstr = ' {} '.format(self.element)
+		return namestr, elemstr
+
+
