a2599bcc44dd2a7d2bcf3b14f5b303b9f3c19c00
tjlaboss <travis@ktc-technology.com>
2021-08-27 08:13:21 -0400

    DAKOTA Deck boilerplate

---
+++ pandakota/input/deck.py
@@ -0,0 +1,25 @@
+"""
+Deck
+
+Class for DAKOTA input decks
+"""
+
+class Deck:
+	"""DAKOTA Input deck builder
+	
+	"""
+	def __init__(self):
+		self._all_variables = set()
+	
+	def __iter__(self):
+		return iter(self._all_variables)
+	
+	def __getitem__(self, item):
+		if item not in self._all_variables:
+			raise KeyError(item)
+		# TODO: get the appropriate variable
+		raise NotImplementedError(f"Deck[{item}]")
+	
+	def items(self):
+		return {k: self[k] for k in self}.items()
+
