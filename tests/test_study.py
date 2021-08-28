f36cbfa31a9c012ed1b4c2eb2c74a1d2f540a8a1
tjlaboss <travis@ktc-technology.com>
2021-08-28 13:50:17 -0400

    Test using a study with a new command

---
+++ tests/test_study.py
@@ -0,0 +1,32 @@
+"""Test the Study class"""
+import io
+import sys
+import shutil
+import pandakota
+
+mc = pandakota.input.methods.MonteCarloSampling(100, 42)
+deck = pandakota.input.Deck(functions=['f'], method=mc)
+deck.add_variable(pandakota.input.NormalUncertainVariable('x', +1.0, 0.05))
+deck.add_variable(pandakota.input.NormalUncertainVariable('y', -1.0, 0.07))
+
+
+class FileIO(io.StringIO):
+	def __init__(self, number, *args, **kwargs):
+		self.number = number
+		super().__init__(*args, **kwargs)
+		
+	def fileno(self) -> int:
+		return self.number
+
+
+def test_echo():
+	sys.stdout = FileIO(1)
+	stu = pandakota.Study(deck, bin_path="echo")
+	stu.run_dakota()
+	solution = "# Usage:\n#   echo -i dak.in -o dak.out -write_restart Restart0.rst"
+	with open("dakota_study/dak.in", 'r') as fin:
+		text = fin.read()
+	assert solution in text, "echo test failed"
+	# TODO: change this once workdir is configurable
+	shutil.rmtree(stu._plot_dir)
+	shutil.rmtree(stu._dakota_dir)
