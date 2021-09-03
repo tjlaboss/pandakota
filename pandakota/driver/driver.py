9f6f592026751f0c449b5b451f357c8edb3b76cc
tjlaboss <travis@ktc-technology.com>
2021-09-03 09:44:19 -0400

    Nice logging streams

--- pandakota/driver/driver.py
+++ pandakota/driver/driver.py
@@ -6,6 +6,8 @@ import io
 import abc
 import typing
 import logging
+import fcntl
+import atexit
 
 
 RESULT_TYPE = typing.Tuple[
@@ -23,15 +25,35 @@ class Driver(abc.ABC):
 	def __init__(self, eval_id, param_dict, **kwargs):
 		self._eval_id = eval_id
 		self.param_dict = param_dict
-		self._logger = None
-		self._logfile = None
-		self._logstream = None
-		self._loghandler = None
+		self._logger: logging.Logger = None
+		self._logfile: str = None
+		self._logstream: io.StringIO = None
+		self._loghandler: logging.StreamHandler = None
 		self._procs = []  # List of objects with a .kill() method.
+		# Remember that atexit is in reverse order.
+		# Do not resiter 'self.kill' here without removing its logging statements.
+		atexit.register(self._write_stream)
 	
 	@property
 	def eval_id(self):
 		return self._eval_id
+	
+	def _write_stream(self):
+		"""Write the stream to the communal log."""
+		if self._logstream is None:
+			return
+		self._logstream.flush()
+		with open(self._logfile, 'a') as f:
+			fcntl.flock(f, fcntl.LOCK_EX)
+			f.write(self._logstream.getvalue())
+			fcntl.flock(f, fcntl.LOCK_UN)
+		self._logstream.close()
+	
+	def _flush_stream(self):
+		"""Renew the logstream. Write to the communal log and start a new one."""
+		self._write_stream()
+		self._logstream = io.StringIO()
+		self._loghandler.setStream(self._logstream)
 	
 	def activate_collective_logging(self, logfile: str, logfmt=None):
 		"""Set up logging for the communal log file."""
